73e222ae2f1ab5dee4a57ed1454a69048da63bb3
Dominic May <jack.thatch@gmail.com>
2014-10-19 04:02:32 +0800

    Cleanup

--- genda/genda/urls.py
+++ genda/genda/urls.py
@@ -12,21 +12,11 @@ v1_api.register(AllPronounsResource())
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'genda.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    # url(r"/login", LoginHandler),
-    # url(r"/logout", LogoutHandler),
-    # url(r"/test", TestHandler),
-    # url(r"/users", UsersHandler),
-    # url(r"/create_user", CreateUserHandler),
-    # url(r"/everything", EverythingHandler)
     url(r'^demo/', demo),
     url(r'^api/', include(v1_api.urls)),
     url(r'^foundation', include(foundation.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r"^", index),
     url(r'^accounts/', include('nopassword.urls')),
     url(r'^profile/$', profile),
+    url(r"^$", index),
 )
