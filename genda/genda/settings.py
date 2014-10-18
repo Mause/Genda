e4a2839244b12aa8ea0ed210ed965a1650b07efb
Dominic May <jack.thatch@gmail.com>
2014-10-19 04:01:16 +0800

    Get templates working properly (maybe?)

--- genda/genda/settings.py
+++ genda/genda/settings.py
@@ -30,6 +30,17 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.request",
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages"
+)
+
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
