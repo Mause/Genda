46f3188bfeb1a50ab89f7bc944a172856c16ffe4
Dominic May <jack.thatch@gmail.com>
2014-10-18 18:50:09 +0800

    Outline Login & Logout handlers

--- src/main.py
+++ src/main.py
@@ -14,14 +14,43 @@ class MainHandler(RequestHandler):
     def get(self):
         self.render('home.html')
 
+
+class LoginHandler(BaseRequestHandler):
+    def get(self):
+        if self.current_user is not None:
+            return self.redirect('/')
+
+        self.render('login.html')
+
+    def post(self):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        password = pbkdf2_sha256.
+
+        if not self.user_exists(username):
+            return
+
+        import IPython
+        IPython.embed()
+
+        self.set_secure_cookie('uid', value)
+
+
+class LogoutHandler(BaseRequestHandler):
+    def get(self):
+        self.set_secure_cookie('uid', '')
+
+
 settings = {
     'debug': True,
-    'template_path': 'templates'
+    'template_path': 'templates',
+    'cookie_secret': 'perthstudenthack'
 }
 
 
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/login", LoginHandler)
 ], **settings)
 
 if __name__ == "__main__":
